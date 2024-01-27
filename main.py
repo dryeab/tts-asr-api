a41dd28505732c17182669212df0fcf7b85dfa83
Yared Tsegaye <yaredtsegaye120@gmail.com>
2024-01-27 10:37:56 +0300

    Add fixes

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import tempfile
-import os
+import uvicorn
 import model
+from pathlib import Path 
 from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...)):
     try:
         # Create a temporary file
-        with tempfile.NamedTemporaryFile(suffix=".wav", delete=True) as tmp_audio_file:
+        custom_temp_dir = Path(__file__).parent.resolve() / "temp_audio"
+        with tempfile.NamedTemporaryFile(suffix=".wav", delete=False, dir=custom_temp_dir) as tmp_audio_file:
             # Write the contents of the uploaded file to the temporary file
             tmp_audio_file.write(await file.read())
             tmp_audio_file.flush()
@@ -22,3 +35,6 @@ async def upload_file(file: UploadFile = File(...)):
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="192.168.0.172", port=8000)
