d9fa4de257b935b36c8b041dc636df146d998d59
Yeabsira <dryeab@gmail.com>
2024-04-07 20:56:25 +0300

    Added error voice

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tempfile
 import uvicorn
 import model
 from pathlib import Path
-from fastapi import FastAPI, File, UploadFile, HTTPException 
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from chatgpt_client import ChatGPTClient
@@ -47,17 +47,13 @@ async def upload_file(file: UploadFile = File(...)):
             transcribed_text = model.asr(tmp_file_path)
 
             response = client.query(transcribed_text)
-            
-            print("Here is the file", tmp_audio_file.name)
-            
+
             model.tts(response.replace("\n", ""), tmp_audio_file.name)
-            
+
             return FileResponse(tmp_audio_file.name, media_type="audio/wav")
 
-            # return {"text": response}
-
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+        return FileResponse(Path(__file__).parent / "error.mp3")
 
 
 if __name__ == "__main__":
